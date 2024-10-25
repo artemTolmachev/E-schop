30189f47754175d39b5cbe53058354e63dee9e66
artem <89ufisum@gmail.com>
2024-10-25 21:06:54 +0200

    section product-info

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,5 @@
 let noticeEl = document.querySelector('.notice');
+let stepperEl = document.querySelector('.stepper');
 
 if(noticeEl){
     const noticeCloseEl = noticeEl.querySelector('.notice__close')
@@ -7,3 +8,56 @@ if(noticeEl){
     })
 }
 
+if(stepperEl){
+    const stepperBtnMinusEl = stepperEl.querySelector('.stepper__btn--minus');
+    const stepperBtnPlusEl = stepperEl.querySelector('.stepper__btn--plus');
+    const stepperInputEl = document.querySelector('.stepper__input');
+
+    let stepperMin = +stepperInputEl.getAttribute('min');
+    let stepperMax = +stepperInputEl.getAttribute('max');
+
+    let count = Number(stepperInputEl.value);
+
+    stepperInputEl.addEventListener('change', () => {
+        stepperBtnMinusEl.classList.remove('stepper__btn--disable');
+        stepperBtnPlusEl.classList.remove('stepper__btn--disable');
+
+        if(stepperInputEl.value <= stepperMin){
+            stepperInputEl.value = stepperMin
+            stepperBtnMinusEl.classList.add('stepper__btn--disable');
+        }
+        if(stepperInputEl.value >= stepperMax){
+            stepperInputEl.value = stepperMax
+            stepperBtnPlusEl.classList.add('stepper__btn--disable');
+        }
+    })
+ 
+
+    stepperBtnPlusEl.addEventListener('click', () => {
+        count = Number(stepperInputEl.value);
+        if(count < stepperMax){
+            stepperBtnMinusEl.classList.remove('stepper__btn--disable');
+            stepperBtnPlusEl.classList.remove('stepper__btn--disable');
+            count++;
+            stepperInputEl.value = count;
+        }
+        if(count === stepperMax){
+            stepperBtnPlusEl.classList.add('stepper__btn--disable');
+        }
+        
+    })
+      
+    stepperBtnMinusEl.addEventListener('click', () => {
+        count = Number(stepperInputEl.value);
+        console.log(stepperMin)
+        if(count > stepperMin){
+            stepperBtnMinusEl.classList.remove('stepper__btn--disable');
+            stepperBtnPlusEl.classList.remove('stepper__btn--disable');
+            count--;
+            stepperInputEl.value = count;
+        }
+        if(count === stepperMin){
+            stepperBtnMinusEl.classList.add('stepper__btn--disable');
+        }
+    })
+}
